c7fa89424b18bada1728c7a24cfcbbbfc8513dea
MaAlina1708 <alinamastega@outlook.com>
2022-08-19 00:16:37 +0300

    Added a search engine

--- src/app.js
+++ src/app.js
@@ -43,8 +43,18 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", `${response.data.weather[0].description}`);
 }
 
-let apiKey = "173ad7f0400a4a67080b878412caf07f";
-let city = "Lisbon";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&&units=metric`;
+function search(city) {
+  let apiKey = "173ad7f0400a4a67080b878412caf07f";
 
-axios.get(apiUrl).then(displayTemperature);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
